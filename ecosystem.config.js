f9cfb5ed0baf3bcea5cc1f368064916fcf942c22
Sami Barasi <sami.barasi@nakedtoast.de>
2021-04-10 07:58:23 +0200

    Py Server

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    apps: [{
+        name: 'python-aws',
+        script: './server/index.js'
+    }],
+    deploy: {
+        production: {
+            user: 'ubuntu',
+            host: 'ec2-34-242-150-148.eu-west-1.compute.amazonaws.com',
+            key: '~/Work/AWS/keys/Irland/tutorial.pem',
+            ref: 'origin/main',
+            repo: 'git@github.com:samibarasi/python-aws.git',
+            path: '/home/ubuntu/python-aws',
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        }
+    }
+}
