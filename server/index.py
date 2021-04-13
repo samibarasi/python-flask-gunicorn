3321deb02eaab1aa326e6f3ed87f70c0b8f24110
Sami Barasi <sami.barasi@nakedtoast.de>
2021-04-13 12:52:24 +0200

    change hello

--- server/index.py
+++ server/index.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return "Hello World"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=4000, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=4000, debug=false, threaded=True)
 
